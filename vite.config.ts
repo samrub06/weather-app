001d8920c4af6e03f05bc8649cca28a2d4e113b9
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2025-06-27 10:28:41 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
